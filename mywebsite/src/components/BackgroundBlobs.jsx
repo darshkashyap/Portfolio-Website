9be8c06ac3b9b118393d2c1fe55011990fb8bbe7
Darsh kashyap <dkdarshkashyap07@gmail.com>
2025-08-08 13:55:55 +0530

    Update BackgroundBlobs.jsx

--- mywebsite/src/components/BackgroundBlobs.jsx
+++ mywebsite/src/components/BackgroundBlobs.jsx
@@ -14,20 +14,20 @@ const floatingVariant = {
 const BackgroundBlobs = () => {
  return (
     <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
-      {/* Blue circle */}
+      
       <motion.div
         className="absolute top-[-100px] left-[-100px] w-96 h-96 rounded-full bg-cyan-500 opacity-20 blur-[120px]"
         variants={floatingVariant}
         animate="animate"
       />
-      {/* Pink ellipse */}
+    
       <motion.div
         className="absolute top-[40%] right-[-120px] w-[500px] h-[300px] bg-pink-500 opacity-20 rounded-full blur-[120px]"
         style={{ transform: 'rotate(45deg)' }}
         variants={floatingVariant}
         animate="animate"
       />
-      {/* Purple circle */}
+     
       <motion.div
         className="absolute bottom-[-150px] left-[20%] w-[400px] h-[400px] bg-purple-600 opacity-20 rounded-full blur-[100px]"
         variants={floatingVariant}
