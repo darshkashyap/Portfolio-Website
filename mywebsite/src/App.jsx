9bba413740625ff34746974955cb3d09007ad716
Darsh kashyap <dkdarshkashyap07@gmail.com>
2025-12-17 03:57:14 +0530

    removed unneccesary code

--- mywebsite/src/App.jsx
+++ mywebsite/src/App.jsx
@@ -4,7 +4,7 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Loading from './components/Loading';
-import BackgroundBlobs from './components/BackgroundBlobs';
+
 
 import Home from './pages/Home';
 import About from './pages/About';
